420abeec290be90b8e6c27fff3a6f8468c6e8905
rafaadail <rafaadailph@gmail.com>
2021-01-31 19:03:32 -0300

    Implementando CRUD REST

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: false
+}));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -12,6 +18,29 @@ const route = router.get('/', (req, res, next) => {
     })
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id, 
+        item: req.body
+    });
+});
+
+const del = router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        id: id,
+        item: req.body
+    });
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
+app.use('/products', del);
 
 module.exports = app;
