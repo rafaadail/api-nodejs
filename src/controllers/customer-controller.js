7ada10aa8e1d86c52ae45b6d1a8cf5b66e506460
rafaadail <rafaadailph@gmail.com>
2021-03-08 21:47:52 -0300

    Implementação de envio de e-mail, lib sandgrid.

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -4,6 +4,8 @@ const repository = require('../repositories/customer-repository');
 const guid = require('guid');
 const md5 = require('md5');
 
+const emailService = require('../services/email-service');
+
 exports.get = async(req, res, next) => {
     try {
         var data = await repository.get();
@@ -22,6 +24,12 @@ exports.post = async(req, res, next) => {
             email: req.body.email,
             password: md5(req.body.password + global.SALT_KEY)
         });
+
+        emailService.send(
+            req.body.email,
+            'Bem vindo ao Node Store',
+            global.EMAIL_TMPL.replace('{0}', req.body.name));
+
         res.status(201).send({ message: 'Cliente cadastrado com sucesso!'});
     } catch(e) {
         res.status(400).send({ 
@@ -30,4 +38,3 @@ exports.post = async(req, res, next) => {
         });
     }
 };
-
