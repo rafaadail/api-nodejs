90dec69b630269695e351a20f21759d474f02c44
rafaadail <rafaadailph@gmail.com>
2021-03-11 20:31:33 -0300

    Definido autenticação para rota de pedido.

--- src/routes/order.route.js
+++ src/routes/order.route.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controller = require('../controllers/order.controller');
 const authService = require('../services/auth-service');
 
-router.get('/', controller.get);
+router.get('/', authService.authorize, controller.get);
 router.post('/', authService.authorize, controller.post);
 
 module.exports = router;
