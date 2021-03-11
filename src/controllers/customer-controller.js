d6096fd5c62714dc3be0db2c2e3543693b2ac514
rafaadail <rafaadailph@gmail.com>
2021-03-11 20:52:15 -0300

    Refresh Token.

--- src/controllers/customer-controller.js
+++ src/controllers/customer-controller.js
@@ -74,3 +74,38 @@ exports.authenticate = async(req, res, next) => {
         });
     }
 };
+
+exports.refreshToken = async(req, res, next) => {
+    try {
+        const token = req.body.token || req.query.token || req.headers['x-access-token'];
+        const data = await authService.decodeToken(token);
+
+        const customer = await repository.getById(data.id);
+
+        if(!customer) {
+            res.status(404).send({
+                message: 'Cliente não encontrado'
+            });
+            return;
+        }
+
+        const tokenData = await authService.generateToken({
+            id: customer._id,
+            email: customer.email, 
+            name: customer.name
+        });
+
+        res.status(201).send({
+            token: tokenData,
+            data: {
+                email: customer.email,
+                name: customer.name
+            }
+        });
+    } catch(e) {
+        res.status(400).send({ 
+            message: 'Falha ao cadastrar o cliente!',
+            data: e
+        });
+    }
+};
