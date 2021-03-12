aae0be6baea67a5fc0d99e2a3e5dc32858345467
rafaadail <rafaadailph@gmail.com>
2021-03-12 11:11:18 -0300

    Reset de config.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
 module.exports = {
-    connectionString: 'mongodb+srv://api-nodejs:api-nodejs@cluster0-cmhn9.mongodb.net/api-nodejs?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=nodejsapi;AccountKey=Y8KjHVbPJoKFXzZvR29EmjfL1rTA50FimHB+ydH6Ztw7lLgq84u/3y3TEhtyrpU+935Jj5S6vUsk+SMfCxJJ8A==;EndpointSuffix=core.windows.net'
+    connectionString: '',
+    sendgridKey: '',
+    containerConnectionString: ''
 }
